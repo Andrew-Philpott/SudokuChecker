482307c124a24484f0b3cd1bdef319c62338f2df
Andrew Philpott and Andriy Veremyeyev <andriy@>
2020-02-04 09:58:52 -0800

    Check to see if the x and y coordinates and value passed into a space object is saved to the space object.

--- __tests__/space.test.js
+++ __tests__/space.test.js
@@ -7,4 +7,19 @@ describe('Space', () => {
     let space = new Space();
       expect(space instanceof Space).toEqual(true);
     });
+
+    test('Should be able to make a space object with x and y coordinates and a number value', () => {
+    let xCoordinateValue = 5;
+    let yCoordinateValue = 6;
+    let valueOfSpace = 4;
+    let space = new Space(xCoordinateValue, yCoordinateValue, 0);
+    //let space = new Space(xCoordinateValue, yCoordinateValue, valueOfSpace);
+    let valueOfXCoordinateFromSpaceObject = space.x;
+    let valueOfYCoordinateFromSpaceObject = space.y;
+    let valueOfValueFromSpaceObject = space.value;
+
+        expect(xCoordinateValue).toEqual(valueOfXCoordinateFromSpaceObject);
+        expect(yCoordinateValue).toEqual(valueOfYCoordinateFromSpaceObject);
+        expect(valueOfSpace).toEqual(valueOfValueFromSpaceObject);
+    });
 });
